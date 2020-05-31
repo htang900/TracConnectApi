14bc0bca12e5f923b2f665bfed93bb83f3607fc3
Henry Tang <htang@tracintermodal.com>
2020-05-31 14:20:48 -0400

    testing 5

--- Controllers/MongoCRUD.cs
+++ Controllers/MongoCRUD.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 
-namespace TracConnectApi
+namespace TracConnectApi.Controllers
 {
     public class MongoCRUD 
     {
--- TracConnectApi/Startup.cs
+++ TracConnectApi/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-//using TracConnectApi.Controllers;
+using TracConnectApi.Controllers;
 //using TracConnectApi.Services;
 
 namespace TracConnectApi
